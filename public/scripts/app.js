c26ae36a555917a2acd2889a2db3319eb5b9e4dc
jamestothe <jamesdelacruz@gmail.com>
2016-10-23 01:21:39 -0700

    converting application images into js arrays

---
+++ public/scripts/app.js
@@ -0,0 +1,5 @@
+console.log("app.js is linked!");
+
+var favoriteTech = ["/public/images/rubyonrails.png", "/public/images/javascript.png", "/public/images/c++.png", "/public/images/angular.png", "/public/images/github.png"];
+
+var otherTech = ["/public/images/html5.png","/public/images/css3.png", "/public/images/jquery.png", "/public/images/mongodb.png", "/public/images/postgresql.png", "/public/images/expressjs.png", "/public/images/nodejs.png", '/public/images/heroku.png', "/public/images/trello.png", "/public/images/balsamiq.png", "/public/images/photoshop.png", "/public/images/illustrator.png", "/public/images/bootstrap.png", "/public/images/materialize.png"]; 
